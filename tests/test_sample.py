29c1f2113e30afc4bab749d1450767070623c1d5
oPromessa <mario@silvapereira.com>
2017-11-20 21:10:42 +0100

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,13 @@
 # content of test_sample.py
+
+from uploadr.py import *
+
 def func(x):
     return x + 1
 
 def test_answer():
     assert func(3) == 4
+    
+def test_isThisStringUnicode():
+    s = isThisStringUnicode(u'Some unicode string')
+    assert s == True
