af79f07e27b5f121e8e4d2ab08c441cbbd4fe756
oPromessa <mario@silvapereira.com>
2017-11-23 20:20:41 +0100

    tests adjusted

--- tests/test_StrUni.py
+++ tests/test_StrUni.py
@@ -3,6 +3,6 @@
 import StrUni
 
 def test_Unicode():
-    for i in range(1,200):
+    for i in range(1,127):
         assert StrUni.isThisStringUnicode(chr(i)) == False
         assert StrUni.isThisStringUnicode(unicode(chr(i).decode('utf-8'))) == True
--- uploadr.py
+++ uploadr.py
@@ -235,12 +235,6 @@ def isThisStringUnicode(s):
                                  if isThisStringUnicode(file) \
                                  else file))
 
-    >>> isThisStringUnicode('No Unicode string')
-    False
-    >>> isThisStringUnicode(u'Unicode string')
-    True
-    >>> isThisStringUnicode(2)
-    False                                 
     """
     if isinstance(s, unicode):
         return True
