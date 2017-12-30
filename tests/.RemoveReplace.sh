f1960cf13870077b0188e50c50b5b03ab211d365
oPromessa <mario@silvapereira.com>
2017-12-30 05:37:46 +0100

    .RemoveReplace edited in GitHub web for unicode characters

--- tests/.RemoveReplace.sh
+++ tests/.RemoveReplace.sh
@@ -1,4 +1,4 @@
 if [[ $1 == RemoveReplace ]]; then rm "./tests/Test Photo Library/photo.jpg"; fi
-if [[ $1 == RemoveReplace ]]; then rm "./tests/Test Photo Library/photo_Excepção.jpg"; fi
-if [[ $1 == RemoveReplace ]]; then cp "./tests/Test Photo Library/ReplaceTest/file_34_Substituição.ForReplace" "./tests/Test Photo Library/Pics.Replaced/file_34_Substituição.jpg"; fi
+if [[ $1 == RemoveReplace ]]; then rm "./tests/Test Photo Library/photo_Excepção.jpg"; fi
+if [[ $1 == RemoveReplace ]]; then cp "./tests/Test Photo Library/ReplaceTest/file_34_Substituição.ForReplace" "./tests/Test Photo Library/Pics.Replaced/file_34_Substituição.jpg"; fi
 if [[ $1 == RemoveReplace ]]; then cp "./tests/Test Photo Library/ReplaceTest/file_35.jpg.ForReplace" "./tests/Test Photo Library/Pics.Replaced/file_35.jpg"; fi
